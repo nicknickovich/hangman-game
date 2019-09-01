3913423a4122467aa2ad97d0e0e989c0985d60f0
nicknickovich <50242565+nicknickovich@users.noreply.github.com>
2019-09-01 09:36:15 +0300

    Add files via upload

--- hangman.py
+++ hangman.py
@@ -10,49 +10,58 @@ from Figure import Figure
 from TextArea import TextArea
 pygame.init()
 
-# set title for the game window
-pygame.display.set_caption("Hangman")
+class Game:
+    def __init__(self):
+        # set title for the game window
+        pygame.display.set_caption("Hangman")
 
-# clock that sets the amount of frames per second
-fps_clock = pygame.time.Clock()
+        # clock that sets the amount of frames per second
+        self.fps_clock = pygame.time.Clock()
 
-settings = Settings()
+        self.settings = Settings()
 
-# create display surface on which everithing is drawn
-DISPLAY_SURFACE = pygame.display.set_mode((settings.WINDOW_WIDTH, 
-                                            settings.WINDOW_HEIGHT))
+        # create display surface on which everithing is drawn
+        self.DISPLAY_SURFACE = pygame.display.set_mode((self.settings.WINDOW_WIDTH, 
+                                                    self.settings.WINDOW_HEIGHT))
 
-# create instance of figure class
-figure = Figure(DISPLAY_SURFACE)
-ta = TextArea(DISPLAY_SURFACE)
+        # create instance of figure class
+        self.figure = Figure(self.DISPLAY_SURFACE)
+        self.ta = TextArea(self.DISPLAY_SURFACE)
 
-while True:
-    DISPLAY_SURFACE.fill(settings.BLACK)
+    def run(self):
+        while True:
+            self.DISPLAY_SURFACE.fill(self.settings.BLACK)
 
-    # check key and mouse events
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            pygame.quit()
-            sys.exit()
-        elif event.type == KEYDOWN and ta.state == "active":
-            if 97 <= event.key <= 122:
-                ta.check_letter(chr(event.key))
-                ta.check_game_state()
-                figure.guesses = len(ta.guessed_wrong)
-        elif event.type == KEYDOWN and ta.state != "active":
-            if event.key == K_RETURN or event.key == K_SPACE:
-                ta.reset_text()
-                figure.reset_figure()
+            # check key and mouse events
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    pygame.quit()
+                    sys.exit()
+                elif event.type == KEYDOWN and self.ta.state == "active":
+                    if 97 <= event.key <= 122:
+                        self.ta.check_letter(chr(event.key))
+                        self.ta.check_game_state()
+                        self.figure.guesses = len(self.ta.guessed_wrong)
+                elif event.type == KEYDOWN and self.ta.state != "active":
+                    if event.key == K_RETURN or event.key == K_SPACE:
+                        self.ta.reset_text()
+                        self.figure.reset_figure()
 
-    if ta.state == "active":
-        ta.display_textarea()
-    elif ta.state == "win":
-        ta.display_win()
-    elif ta.state == "lost":
-        ta.display_lost()
+            if self.ta.state == "active":
+                self.ta.display_textarea()
+            elif self.ta.state == "win":
+                self.ta.display_win()
+            elif self.ta.state == "lost":
+                self.ta.display_lost()
 
-    figure.draw_figure()
+            self.figure.draw_figure()
+            
+            pygame.display.update()
+            self.fps_clock.tick(self.settings.FPS)
     
-    pygame.display.update()
-    fps_clock.tick(settings.FPS)
-    
+def main():
+    game = Game()
+    game.run()
+
+if __name__ == "__main__":
+    main()
